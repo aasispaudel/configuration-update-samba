6890dd031539980f8a23290df241b57e01d3c669
Aasis <aasispaudelthp2@gmail.com>
2019-03-31 14:11:03 +0200

    first commit

---
+++ samba_d.py
@@ -0,0 +1,29 @@
+#!usr/bin/env python3
+
+import configparser
+import subprocess
+
+'''
+Installs samba
+Change the .conf files with necessary changes
+'''
+
+'''
+@bug does not save the comments of original .conf file
+'''
+
+samba_config_path = '/etc/samba/smb.conf'
+
+subprocess.call('sudo apt-get install samba'.split())
+config = configparser.ConfigParser()
+config.read(samba_config_path)
+
+config.set('global', 'logging', 'file')
+config.set('global', 'log level', '2')
+
+hostname = subprocess.check_output('hostname').decode('utf-8')
+config.set('global', 'netbios name', hostname)
+
+with open(samba_config_path, 'w') as f:
+    config.write(f)
+
